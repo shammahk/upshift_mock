9a10bb3efecafeef5510832b417ae8881dfea5c3
shammahk <shammahk1@gmail.com>
2019-05-02 20:33:52 +0100

    completed design

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,13 @@
-var header = document.querySelector("header");
+// var header = document.querySelector("header");
 
-var window = document.querySelector(Window);
-window.addEventListener("onscroll", onStick);
+// window.addEventListener("scroll", onStick);
 
-function onStick() {
-    header.style.position = 'sticky';
-    header.style.borderBottom = 'solid 1px '
+// function onStick() {
+//     header.style.position = 'fixed';
+//     header.style.borderBottom = 'solid 0.1px rgba(72, 137, 239, 0.3)';
+//     header.style.zIndex = '1';
+//     header.style.paddingBottom= '10px;'
+//     header.style.background = 'rgba(185, 143, 69, 0.3)';
+//     console.log('working');
     
-}
+// }
